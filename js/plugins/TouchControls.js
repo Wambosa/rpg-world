7d2eedf6d2adc9dfc0db406502023cc94d2ca074
Jorrell <kevinowen11@gmail.com>
2017-05-16 03:39:27 +0000

    move while press and hold test

---
+++ js/plugins/TouchControls.js
@@ -0,0 +1,72 @@
+Phaser.Plugin.TouchControls = function(game, parent) {
+    Phaser.Plugin.call(this, game, parent);
+    
+    //custom init logic
+    this.game = game;
+    
+};
+
+Phaser.Plugin.TouchControls.prototype = Object.create(Phaser.Plugin.prototype);
+Phaser.Plugin.TouchControls.prototype.constructor = Phaser.Plugin.TouchControls;
+
+Phaser.Plugin.TouchControls.prototype.setup = function(player, buttons){
+    this.player = player;
+    
+    //object to track which buttons are pressed
+    this.player.btnsPressed = this.player.btnsPressed || {};
+    
+    //size relative to screen
+    this.btnH = 0.08 * this.game.width;
+    this.butnW = this.btnH;
+    this.edgeDistance = 0.25 * this.btnH;
+    this.sizeActionBtn = 1.5 * this.btnH;
+    
+    //button positions
+    var leftX = this.edgeDistance;
+    var leftY = this.game.height - this.edgeDistance - this.btnW - this.btnH;
+    
+    var rightX = this.edgeDistance + this.btnH + this.btnW;
+    var rightY = this.game.height - this.edgeDistance - this.btnW - this.btnH;
+    
+    var upX = this.edgeDistance + this.btnW + this.btnH;
+    var upY = this.game.height - this.edgeDistance - 2 * this.btnW - this.btnH;
+    
+    var downX = this.edgeDistance + this.btnW + this.btnH;
+    var downY = this.game.height - this.edgeDistance - this.btnW;
+    
+    //buttons are bitmaps
+    this.directionBitmap = this.game.add.bitmapData(this.btnW, this.btnH);
+    this.directionBitmap.ctx.fillStyle = '#4BAFE3';
+    this.directionBitmap.ctx.fillRect(0,0, this.btnW, this.btnH);
+    
+    //left arrow button
+    if(buttons.left){
+        this.leftArrow = this.game.add.button(leftX, leftY, this.directionBitmap);
+        this.leftArrow.alpha = 0.5;
+        this.leftArrow.fixedToCamera = true;
+        
+        this.leftArrow.events.onInputDown.add(function(){
+            this.player.btnsPressed.left = true;
+        }, this);
+        
+        this.leftArrow.events.onInputUp.add(function(){
+            this.player.btnsPressed.left = false;
+        }, this);
+        
+        this.leftArrow.events.onInputOver.add(function(){
+            this.player.btnsPressed.left = true;
+        }, this);
+        
+        this.leftArrow.events.onInputOut.add(function(){
+            this.player.btnsPressed.left = false;
+        }, this);
+        
+        
+        
+    }
+    
+    
+    //events
+    
+};
+
--- js/states/Game.js
+++ js/states/Game.js
@@ -8,18 +8,76 @@ RPG.GameState = {
 
     //constants
     this.PLAYER_SPEED = 90;
+    this.playerScale = 0.8;
     
     //no gravity in a top-down game
     this.game.physics.arcade.gravity.y = 0;    
 
     //keyboard cursors
     this.cursors = this.game.input.keyboard.createCursorKeys();
+    this.wasd = {
+      up: RPG.game.input.keyboard.addKey(Phaser.Keyboard.W),
+      down: RPG.game.input.keyboard.addKey(Phaser.Keyboard.S),
+      left: RPG.game.input.keyboard.addKey(Phaser.Keyboard.A),
+      right: RPG.game.input.keyboard.addKey(Phaser.Keyboard.D),
+    };
+    
   },
   create: function() {   
+    
+    this.game.touchControls = this.game.plugins.add(Phaser.Plugin.TouchControls);
 
     this.loadLevel();
   },   
-  update: function() {    
+  update: function() {  
+
+    
+    this.game.physics.arcade.collide(this.player, this.collisionLayer);
+    this.game.physics.arcade.collide(this.player, this.interactiveCollisionLayer);
+    
+    //touch movement
+
+    this.player.body.drag.x = 200;
+    this.player.body.drag.y = 200;
+ 
+    if(this.game.input.activePointer.isDown){
+      var x = this.game.input.activePointer.worldX;
+      var y = this.game.input.activePointer.worldY;
+      if(this.player.x < x - 10){
+        this.player.body.velocity.x = 90;
+      }else if(this.player.x > x + 10){
+        this.player.body.velocity.x = -90;
+      }
+      
+      if (this.player.y < y - 10){
+        this.player.body.velocity.y = 90;
+      }else if(this.player.y > y + 10){
+        this.player.body.velocity.y = -90;
+      }
+    }
+    
+    
+    //wasd key movement
+    if(this.wasd.down.isDown){
+      this.player.body.velocity.y = this.PLAYER_SPEED;
+      this.player.play('walk');
+    }else if(this.wasd.up.isDown){
+      this.player.body.velocity.y = -this.PLAYER_SPEED;
+      this.player.play('walk');
+    }
+    
+    if(this.wasd.right.isDown){
+      this.player.body.velocity.x = this.PLAYER_SPEED;
+      this.player.play('walk');
+      this.player.scale.setTo(-this.playerScale, this.playerScale);
+    }else if(this.wasd.left.isDown){
+      this.player.body.velocity.x = -this.PLAYER_SPEED;
+      this.player.play('walk');
+      this.player.scale.setTo(this.playerScale);
+    }
+    
+    
+    
     
   },     
   loadLevel: function(){
@@ -31,18 +89,67 @@ RPG.GameState = {
     
     //create tile layers
     this.backgroundLayer = this.map.createLayer('backgroundLayer');
+    this.backgroundLayer.inputEnabled = true;
+    //this.backgroundLayer.events.onInputDown.add(this.movePlayer, this);
     this.collisionLayer = this.map.createLayer('collisionLayer');
+    this.interactiveLayer = this.map.createLayer('interactiveLayer');
+    this.interactiveCollisionLayer = this.map.createLayer('interactiveCollisionLayer');
     
     //send background to the back
     this.game.world.sendToBack(this.backgroundLayer);
     
     //collision layer should be collisionLayer
     this.map.setCollisionBetween(1,16, true, 'collisionLayer');
+    this.map.setCollisionBetween(1,16, true, 'interactiveCollisionLayer');
+    this.map.setCollisionBetween(1,16, true, 'interactiveLayer');
+    
     
     //resize the world to fit the layer
     this.collisionLayer.resizeWorld();
+    
+    //create player
+    
+    var playerData = {
+      //list of items
+      items:[],
+      
+      //stats
+      health: 25,
+      attack: 12, 
+      defense: 8,
+      gold: 100,
+      
+      //quest
+      quests: []
+      
+    };
+    
+    this.player = new RPG.Player(this, 80, 400, playerData);
+    //add player to world
+    this.add.existing(this.player);
+    this.game.camera.follow(this.player);
+    //this.initGUI();
   },
   gameOver: function() {
     this.game.state.start('Game', true, false, this.currentLevel);
+  },
+  movePlayer: function(sprite, event){
+    
   }
+/*
+  initGUI: function(){
+    //touch controls setup
+    this.game.touchControls.setup(this.player, {
+      left: true,
+      right: true,
+      up: true,
+      down: true,
+      upleft: true,
+      downleft: true,
+      upright: true,
+      downright: true,
+      action: true
+    });
+  }
+  */
 };
